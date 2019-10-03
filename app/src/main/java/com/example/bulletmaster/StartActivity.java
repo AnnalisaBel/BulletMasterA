58ec6d7cd38e24bf5c743bbd05783d593ee9010b
Annalisa <annalisa.belia@libero.it>
2019-10-03 11:16:22 +0200

    change

--- app/src/main/java/com/example/bulletmaster/StartActivity.java
+++ app/src/main/java/com/example/bulletmaster/StartActivity.java
@@ -2,7 +2,10 @@ package com.example.bulletmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button butstartgame = findViewById(R.id.butstartgame);
+        butstartgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intgotolev = new Intent(StartActivity.this, LevelsActivity.class);
+                startActivity(intgotolev);
+            }
+        });
     }
 }
